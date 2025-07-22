02bd0583dc6f087b79c6939caab69f816b6aa32f
Snipavn <108266321+Snipavn@users.noreply.github.com>
2025-07-22 13:08:06 +0700

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+ROOTFS_DIR=$(pwd)
+export PATH=$PATH:~/.local/usr/bin
+
+max_retries=50
+timeout=1
+
+ARCH=$(uname -m)
+case $ARCH in
+  x86_64) ARCH_ALT=amd64 ;;
+  aarch64) ARCH_ALT=arm64 ;;
+  *)
+    echo "Unsupported CPU architecture: $ARCH"
+    exit 1
+    ;;
+esac
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  
+wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
+"http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  mkdir -p $ROOTFS_DIR/usr/local/bin
+  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/dxomg/vpsfreepterovm/main/proot-${ARCH}"
+
+  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
+    rm $ROOTFS_DIR/usr/local/bin/proot -rf
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/dxomg/vpsfreepterovm/main/proot-${ARCH}"
+
+    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
+      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+      break
+    fi
+
+    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+    sleep 1
+  done
+
+  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+
+  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
+  rm -rf /tmp/rootfs.tar.xz /tmp/sbin
+  touch $ROOTFS_DIR/.installed
+fi
+
+clear
+echo "SERVER_TIPAC_VN"
+echo "------------------------"
+
+$ROOTFS_DIR/usr/local/bin/proot \
+  --rootfs="${ROOTFS_DIR}" \
+  -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
